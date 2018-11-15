65cb04da8d72cf6106084c16bcc60d26e1735dae
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-11-15 09:45:23 +0000

    Removed deployment, replaced by just pod

--- create_gogs.sh
+++ create_gogs.sh
@@ -9,14 +9,8 @@
 # - meta.namespace
 # - meta.name (from the name of the CR)
 
-#ansible-playbook ./playbook_old.yaml -e "postgresql_port=5432" \
-  # -e "postgresql_volume_size=2Gi" -e "gogs_volume_size=3Gi" \
-  # -e "gogs_port=3000" -e '{"meta": {"namespace":"wkgogs","name":"gogs1"}}'
-
-ansible-playbook ./playbook.yaml -e "postgresql_port=5432" \
-  -e "postgresql_volume_size=2Gi" -e "gogs_volume_size=3Gi" \
-  -e "gogs_port=3000" -e '{"meta": {"namespace":"wkgogs","name":"gogs1"}}' -e "gogs_ssl=True"
-
-# ansible-playbook ./playbook.yaml -e "postgresql_port=5433" \
-#   -e "postgresql_volume_size=2Gi" -e "gogs_volume_size=3Gi" \
-#   -e "gogs_port=3001" -e '{"meta": {"namespace":"wkgogs","name":"gogs2"}}'
+ansible-playbook ./playbook.yaml \
+  -e "postgresql_volume_size=2Gi" \
+  -e "gogs_volume_size=3Gi" \
+  -e "gogs_ssl=True" \
+  -e '{"meta": {"namespace":"wkgogs","name":"gogs1"}}'
