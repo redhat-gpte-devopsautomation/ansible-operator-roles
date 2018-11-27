30cbde27d897e1deb135b21b958dba1e6801a2fd
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-11-27 14:42:19 -0500

    Minor updates for ports etc.

--- create_gogs.sh
+++ create_gogs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # The playbook expects the following variables to be set
-# - postgresql_port
 # - postgresql_volume_size
-# - gogs_port
 # - gogs_volume_size
+# - gogs_ssl
 
-# The following variables come from the ansible-operator
+# The following variables would from the ansible-operator
 # - meta.namespace
 # - meta.name (from the name of the CR)
+# Set to valid values when running the playbook outside of the operator
 
 ansible-playbook ./playbook.yaml \
   -e "postgresql_volume_size=2Gi" \
