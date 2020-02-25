8216f3ac99899d4403e958ca7a9891d853bd4001
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2020-02-25 15:57:54 -0500

    Removed Jenkins

--- delete_jenkins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# The following variables would from the ansible-operator
-# - meta.namespace
-# - meta.name (from the name of the CR)
-# Set to valid values when running the playbook outside of the operator
-
-ansible-playbook ./playbooks/jenkins.yaml \
-  -e "_jenkins_state=absent" \
-  -e '{"meta": {"namespace":"xyz-jenkins","name":"jenkins"}}'
